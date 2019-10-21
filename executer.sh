54dfb96ecd4b55d6012bad4e91218724ede0a2a5
Pedro <phaquino1998@gmail.com>
2019-10-21 16:57:58 -0300

    outputs in .csv instead of .cir.csv

--- executer.sh
+++ executer.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
+
 echo
 echo "Simulation executer"
 for i in $(find . -name "*meas*.cir")
 do
 echo "Simulating: $i"
-hspice $i -o OUTPUT_DATA/$i.csv
+hspice $i -o OUTPUT_DATA/${i%.*}.csv
 done
 
 echo "Done."
