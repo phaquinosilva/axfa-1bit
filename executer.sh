b065ae9d8d3004f673232c24c2f7b57e6392e88d
Pedro <phaquino1998@gmail.com>
2019-10-22 10:01:58 -0300

    corrected spice files

--- executer.sh
+++ executer.sh
@@ -5,7 +5,7 @@ echo "Simulation executer"
 for i in $(find . -name "*meas*.cir")
 do
 echo "Simulating: $i"
-hspice $i -o OUTPUT_DATA/${i%.*}.csv
+hspice $i #! -o OUTPUT_DATA/${i%.*}.csv
 done
 
 echo "Done."
