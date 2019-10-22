703b459dc70c9c4e94c02d39e10f9d959c0ee7a2
Pedro <phaquino1998@gmail.com>
2019-10-22 10:09:48 -0300

    corrections

--- executer.sh
+++ executer.sh
@@ -2,10 +2,17 @@
 
 echo
 echo "Simulation executer"
+
 for i in $(find . -name "*meas*.cir")
 do
-echo "Simulating: $i"
-hspice $i #! -o OUTPUT_DATA/${i%.*}.csv
+  echo "Simulating: $i"
+  hspice $i #! -o OUTPUT_DATA/${i%.*}.csv
+done
+
+echo "Moving results to OUTPUT_DATA"
+for i in $(find . -name "*.csv")
+do
+  mv $i OUTPUT_DATA
 done
 
 echo "Done."
