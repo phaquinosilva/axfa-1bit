9c7107ae99cc04b182a88bbd0b6e5356513d2ea1
Pedro <phaquino1998@gmail.com>
2019-11-07 17:54:18 -0300

    bug fix

--- executer_NT.sh
+++ executer_NT.sh
@@ -14,9 +14,8 @@ do
     NEXT_VOLT=$((VOLT - 1))                                 # define tensão para a nova simulação
     sed 's/vdd = 0.'"$VOLT"'V/vdd = 0.'"$NEXT_VOLT"'V'      # substitui a tensão antiga pela nova
     VOLT=$NEXT_VOLT                                          # define tensão atual como a nova tensão
-    hspice ${i%.*}.cir                                      # roda simulação
-    if [grep -q failed ${i%.*}.csv != 0];
-    then
+    hspice ${i%.*}.cir;                                      # roda simulação
+    if grep -q failed "${i%.*}.mt0.csv"; then
       NEXT_INTERVAL=$((INTERVAL+1))                         # define o novo intervalo
       sed 's/m = '"$INTERVAL"'n/m = '"$NEXT_INTERVAL"'n'    # substitui o período antigo pelo novo
       INTERVAL=$NEXT_INTERVAL;                               # define o intervalo atual como o novo intervalo
